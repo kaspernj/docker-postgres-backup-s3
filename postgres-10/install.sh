30636f217e997381cb748daf7d198e78f4fa6f3a
kaspernj <kaspernj@gmail.com>
2020-09-11 11:51:44 +0200

    Started working on the project

--- postgres-10/install.sh
+++
@@ -1,1 +0,0 @@
-https://raw.githubusercontent.com/schickling/dockerfiles/master/postgres-backup-s3/install.sh
--- postgres-10/run.sh
+++ postgres-10/run.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [ "${S3_S3V4}" = "yes" ]; then
-    aws configure set default.s3.signature_version s3v4
+  aws configure set default.s3.signature_version s3v4
 fi
 
 if [ "${SCHEDULE}" = "**None**" ]; then
